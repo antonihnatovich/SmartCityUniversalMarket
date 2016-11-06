9c80ee05573b96914b29c0437b5eca256d500a0f
Hello Wordovic <tf.2.98@mail.ru>
2016-11-06 16:13:11 +0300

    lil changes

--- SmartCityUniversalMarket/app/src/main/java/com/l/smartcityuniversalmarket/ProductActivity.java
+++ SmartCityUniversalMarket/app/src/main/java/com/l/smartcityuniversalmarket/ProductActivity.java
@@ -2,12 +2,53 @@ package com.l.smartcityuniversalmarket;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.ExpandableListView;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class ProductActivity extends AppCompatActivity {
+
+    private ExpandableListView expandableListView;
+    private List<String> objectsOfType;
+    private Map<String, List<String>> types;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_product);
+        expandableListView = (ExpandableListView) findViewById(R.id.expandableListView);
+        fillData();
+
+    }
+
+    private void fillData() {
+        int counter = 0;
+        List<String[]> tmp = new ArrayList<>();
+        List<String> list = new ArrayList<>();
+        objectsOfType = new ArrayList<>();
+        types = new HashMap<>();
+        String mass[] = getResources().getStringArray(R.array.types),
+                food[] = getResources().getStringArray(R.array.products),
+                clearness[] = getResources().getStringArray(R.array.cleaners),
+                electronic[] = getResources().getStringArray(R.array.electronics),
+                books[] = getResources().getStringArray(R.array.papers);
+
+        tmp.add(food);
+        tmp.add(clearness);
+        tmp.add(electronic);
+        tmp.add(books);
+
+        for (String param : mass)
+            objectsOfType.add(param);
+
+        for (String[] mas : tmp) {
+            for (String toAdd : mas)
+                list.add(toAdd);
+            types.put(objectsOfType.get(counter++), list);
+            list.clear();
+        }
     }
 }
--- SmartCityUniversalMarket/app/src/main/java/com/l/smartcityuniversalmarket/SettingsActivity.java
+++ SmartCityUniversalMarket/app/src/main/java/com/l/smartcityuniversalmarket/SettingsActivity.java
@@ -1,13 +1,45 @@
 package com.l.smartcityuniversalmarket;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
-public class SettingsActivity extends AppCompatActivity {
+public class SettingsActivity extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
+        Spinner spinnerSize = (Spinner) findViewById(R.id.sa_spinner_size);
+        Spinner spinnerFont = (Spinner) findViewById(R.id.sa_spinner_font);
+        findViewById(R.id.sa_button_apply).setOnClickListener(this);
+        findViewById(R.id.sa_button_cancel).setOnClickListener(this);
+
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_spinner_item, getResources().getStringArray(R.array.size));
+        arrayAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinnerSize.setAdapter(arrayAdapter);
+
+        arrayAdapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_spinner_item, getResources().getStringArray(R.array.font));
+        arrayAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinnerFont.setAdapter(arrayAdapter);
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.sa_button_apply:{
+                startActivity(new Intent(this, MainActivity.class));
+                break;
+            }
+            case R.id.sa_button_cancel:{
+                startActivity(new Intent(this, MainActivity.class));
+                break;
+            }
+        }
     }
 }
